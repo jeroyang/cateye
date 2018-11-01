c77e0672b1ce16660c0d390cd3fb62cc20fe93a3
jeroyang <jeroyang@gmail.com>
2018-11-01 10:36:54 +0800

    decrease size

--- setup.py
+++ setup.py
@@ -3,19 +3,16 @@
 from setuptools import setup
 import sys
 import os
-from pipenv.project import Project
-from pipenv.utils import convert_deps_to_pip
 
-with open('README.md') as readme_file:
-    readme = readme_file.read()
+with open('README.md') as f:
+    readme = f.read()
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
 
 if sys.argv[-1] == 'publish':
     os.system("python setup.py sdist bdist_wheel upload")
     sys.exit()
-
-pfile = Project(chdir=False).parsed_pipfile
-requirements = convert_deps_to_pip(pfile['packages'], r=False)
-test_requirements = convert_deps_to_pip(pfile['dev-packages'], r=False)
 
 setup(
     name='cateye',
